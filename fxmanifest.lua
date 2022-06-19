10cefd8321e17df27abfa9b24a4cadf652add314
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-06-19 11:45:48 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,11 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'RexshackGaming <rexshackuk@gmail.com>'
-description 'RexshackGaming : Storage Lockers'
+author 'RexshackGaming <rexshackuk@gmail.com> & Andyuk'
+description 'RexshackGaming & Andyuk : Peds'
 version '1.0.0'
 
 client_script {
-	'client/client.lua',
 	'client/npc.lua',
 	'config.lua'
 }
