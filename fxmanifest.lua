b5a7ec2fd04bf6391d98f525dad1af5bfb1b040a
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-07-18 14:25:33 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'RexshackGaming <rexshackuk@gmail.com> & Andyuk'
-description 'RexshackGaming & Andyuk : Peds'
+author 'Andyuk & RexshackGaming'
+description 'Andyuk : Peds'
 version '1.0.0'
 
 client_script {
