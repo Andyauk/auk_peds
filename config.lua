75adff6a88d02ad55416d40ff4b7272d1ea450c9
Andyauk <47583709+Andyauk@users.noreply.github.com>
2022-07-18 12:53:19 +0100

    Update config.lua
    Added, bank, hospitals, circus, fishing shop, casino. 
    and clean up.

--- config.lua
+++ config.lua
@@ -1,8 +1,7 @@
 Config = {}
 
 Config.peds = {
-	
-	--General Store
+	-- General Store
 	[1] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(1329.84, -1294.29, 77.02 -1), ["Heading"] = 73.02 }, -- rhodes
 	[2] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(-324.12, 803.44, 117.88 -1), ["Heading"] = 275.36 }, -- valentine
 	[3] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(-1789.58, -387.92, 160.33 -1), ["Heading"] = 58.91 }, -- strawberry
@@ -12,33 +11,57 @@ Config.peds = {
 	[7] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(-3687.35, -2623.33, -13.43 -1), ["Heading"] = 264.78 }, -- armadillo
 	[8] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(-784.76, -1322.13, 43.88 -1), ["Heading"] = 184.12 }, -- blackwater
 	[9] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(3025.49, 561.9, 44.72 -1), ["Heading"] = 257.37 }, -- van horn
-	[10] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(-6055.76, -4222.02, -14.15 -1), ["Heading"] = 70.31 }, -- mexico hotel
-	[11] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(-3984.73, -4134.87, -18.51 -1), ["Heading"] = 250.07 }, -- mexico bridge
-	[12] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(-2134.69, -3412.09, 38.69 -1), ["Heading"] = 41.47 }, -- mexico fort
-	[13] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(1175.31, -7243.5, 69.81 -1), ["Heading"] = 337.08 }, -- Guarma fort
-	--Gunsmith
-	[14] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(-280.97, 778.88, 119.5 -1), ["Heading"] = 356.41 }, -- valentine
-	[15] = { ["Model"] = "U_M_M_NbxGunsmith_01", ["Pos"] = vector3(-5506.39, -2963.97, -0.64 -1), ["Heading"] = 100.69 }, -- tumbleweed
-	[16] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(2717.23, -1286.86, 49.64 -1), ["Heading"] = 30.13 }, -- saint denis
-	[17] = { ["Model"] = "U_M_M_NbxGunsmith_01", ["Pos"] = vector3(1322.55, -1323.17, 77.89 -1), ["Heading"] = 348.51 }, -- rhodes
-	[18] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(2948.48, 1319.57, 44.82 -1), ["Heading"] = 69.19 }, -- annesburg
-	--ammosmith
-	[19] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(-2886.15, -5011.47, -10.44 -1), ["Heading"] = 293.62 }, -- mexico
-	[20] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(1469.96, -7119.07, 77.38 -1), ["Heading"] = 201.11 }, -- guarma
-	--Saloon
-	[21] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(-313.26, 805.12, 118.98 -1), ["Heading"] = 279.15 }, -- valentine
-	[22] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(-5518.3, -2906.41, -1.75 -1), ["Heading"] = 219.91 }, -- tumbleweed
-	[23] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(-3699.77, -2594.97, -13.32 -1), ["Heading"] = 88.02 }, -- armadillo
-	[24] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(-817.8, -1319.32, 43.68 -1), ["Heading"] = 279.95 }, -- blackwater
-	[25] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(1340.21, -1374.77, 80.48 -1), ["Heading"] = 263.45 }, -- rhodes
-	[26] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(2792.69, -1167.91, 47.93 -1), ["Heading"] = 263.45 }, -- saint denis
-	[27] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(2948.26, 528.13, 45.34 -1), ["Heading"] = 176.22 }, -- van horn
-	[28] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(1450.98, 369.38, 89.89 -1), ["Heading"] = 87.73 }, -- emerald ranch
-        [29] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(1299.21, -6808.68, 43.64 -1), ["Heading"] = 158.54 }, --guarma
-	--Hardware
-	[30] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(1420.69, 381.19, 90.33 -1), ["Heading"] = 169.56 }, -- valentine	
-        --Blackmarket
-	[31] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(1320.5, -1142.48, 82.35 -1), ["Heading"] = 255.14 }, -- rhodes	
-	[32] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(-4987.44, -4807.32, -9.79 -1), ["Heading"] = 57.07 }, -- mexico	
-	[33] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(2923.09, 1351.29, 44.86 -1), ["Heading"] = 284.07 }, --  annesburg
+	[10] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(-2134.69, -3412.09, 38.69 -1), ["Heading"] = 41.47 }, -- mexico fort
+	[11] = { ["Model"] = "U_M_M_SDEXOTICSSHOPKEEPER_01", ["Pos"] = vector3(1175.31, -7243.5, 69.81 -1), ["Heading"] = 337.08 }, -- Guarma fort
+	-- Gunsmith
+	[12] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(-280.97, 778.88, 119.5 -1), ["Heading"] = 356.41 }, -- valentine
+	[13] = { ["Model"] = "U_M_M_NbxGunsmith_01", ["Pos"] = vector3(-5506.39, -2963.97, -0.64 -1), ["Heading"] = 100.69 }, -- tumbleweed
+	[14] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(2717.23, -1286.86, 49.64 -1), ["Heading"] = 30.13 }, -- saint denis
+	[15] = { ["Model"] = "U_M_M_NbxGunsmith_01", ["Pos"] = vector3(1322.55, -1323.17, 77.89 -1), ["Heading"] = 348.51 }, -- rhodes
+	[16] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(2948.48, 1319.57, 44.82 -1), ["Heading"] = 69.19 }, -- annesburg
+	-- Ammosmith
+	[17] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(-2886.15, -5011.47, -10.44 -1), ["Heading"] = 293.62 }, -- mexico
+	[18] = { ["Model"] = "U_M_M_AsbGunsmith_01", ["Pos"] = vector3(1469.96, -7119.07, 77.38 -1), ["Heading"] = 201.11 }, -- guarma
+	-- Saloon
+	[19] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(-313.26, 805.12, 118.98 -1), ["Heading"] = 279.15 }, -- valentine
+	[20] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(-5518.3, -2906.41, -1.75 -1), ["Heading"] = 219.91 }, -- tumbleweed
+	[21] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(-3699.77, -2594.97, -13.32 -1), ["Heading"] = 88.02 }, -- armadillo
+	[22] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(-817.8, -1319.32, 43.68 -1), ["Heading"] = 279.95 }, -- blackwater
+	[23] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(1340.21, -1374.77, 80.48 -1), ["Heading"] = 263.45 }, -- rhodes
+	[24] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(2792.69, -1167.91, 47.93 -1), ["Heading"] = 263.45 }, -- saint denis
+	[25] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(2639.89, -1224.78, 53.38 -1), ["Heading"] = 91.69 }, -- saint denis2	
+	[26] = { ["Model"] = "U_M_O_ValBartender_01", ["Pos"] = vector3(2948.26, 528.13, 45.34 -1), ["Heading"] = 176.22 }, -- van horn
+	[27] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(1450.98, 369.38, 89.89 -1), ["Heading"] = 87.73 }, -- emerald ranch
+        [28] = { ["Model"] = "U_F_M_TljBartender_01", ["Pos"] = vector3(1299.21, -6808.68, 43.64 -1), ["Heading"] = 158.54 }, --guarma
+	-- Hardware
+	[29] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(1420.69, 381.19, 90.33 -1), ["Heading"] = 169.56 }, -- valentine	
+        -- Blackmarket
+	[30] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(1320.5, -1142.48, 82.35 -1), ["Heading"] = 255.14 }, -- rhodes	
+	[31] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(-4987.44, -4807.32, -9.79 -1), ["Heading"] = 57.07 }, -- mexico	
+	[32] = { ["Model"] = "U_M_O_VhTExoticShopkeeper_01", ["Pos"] = vector3(2923.09, 1351.29, 44.86 -1), ["Heading"] = 284.07 }, --  annesburg
+	-- Casino
+	[33] = { ["Model"] = "U_M_M_NbxBoatTicketSeller_01", ["Pos"] = vector3(2865.46, -1398.92, 43.53 -1), ["Heading"] = 233.96 }, -- casino
+	-- Fishing shop
+	[34] = { ["Model"] = "U_M_M_NbxBankerBounty_01", ["Pos"] = vector3(2499.06, -420.0, 44.37 -1), ["Heading"] = 104.08 }, -- bluewater marsh
+	-- Circus
+	[35] = { ["Model"] = "CS_EscapeArtistAssistant", ["Pos"] = vector3(-1754.91, -1626.95, 91.2 -1), ["Heading"] = 166.07 }, -- thrown
+        -- Hospitals
+	[36] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(-287.97, 804.12, 119.39 -1), ["Heading"] = 278.84 }, -- Valentine
+	[37] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(2381.59, -1370.18, 46.55 -1), ["Heading"] = 239.23 }, -- Saint Denis
+	[38] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(-3648.05, -2651.59, -13.46 -1), ["Heading"] = 39.64 }, -- Armadillo
+	[39] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(1384.91, -7002.14, 57.1 -1), ["Heading"] = 239.23 }, -- Guarma
+	[40] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(-836.75, -1263.61, 43.69 -1), ["Heading"] = 186.62 }, -- Blackwater
+	[41] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(-1806.51, -429.88, 158.83 -1), ["Heading"] = 252.49 }, -- Strawberry
+	[42] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(1200.9, -1231.66, 74.44 -1), ["Heading"] = 86.21 }, -- Rhodes
+	[43] = { ["Model"] = "CS_nbxreceptionist_01", ["Pos"] = vector3(2881.78, 1469.5, 68.76 -1), ["Heading"] = 118.76 }, -- annesburg
+	-- Bank
+	[44] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(1291.16748046875, -1303.3929443359375, 77.04120635986328 -1), ["Heading"] = -39.15393829345703 }, -- Rhodes
+	[45] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(1292.7667236328125, -1304.8292236328125, 77.04267883300781 -1), ["Heading"] = -38.27278900146484 }, -- Rhodes
+	[46] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(2644.82470703125, -1293.88623046875, 52.24774932861328 -1), ["Heading"] = 24.46722221374511 }, -- Saint Denis
+	[47] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(2650.200927734375, -1305.304443359375, 52.26079940795898 -1), ["Heading"] = 26.54940414428711 }, -- Saint Denis
+	[48] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(-308.1111145019531, 773.9920043945312, 118.70309448242188 -1), ["Heading"] = 8.93415641784668 }, -- Valentine
+	[49] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(-306.2430114746094, 773.378662109375, 118.70309448242188 -1), ["Heading"] = -33.39679336547851 }, -- Valentine
+	[50] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(-820.9907836914062, -1278.8634033203125, 43.63804626464844 -1), ["Heading"] = -71.70177459716797 }, -- Blackwater
+	[51] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(-813.1812744140625, -1275.3433837890625, 43.63771438598633 -1), ["Heading"] = 179.87091064453125 }, -- Blackwater
+	[52] = { ["Model"] = "S_M_M_BankClerk_01", ["Pos"] = vector3(-3666.1162109375, -2628.576171875, -13.58811855316162 -1), ["Heading"] = 6.61411952972412 }, -- Armadillo
 }
